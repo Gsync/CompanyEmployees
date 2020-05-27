9f147eaf26095e5da2eefff89ea3afe8ac990a1e
Khuram Niaz <khuram.niaz@vergeag.com>
2020-05-27 17:32:10 -0600

    initial commit

---
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CompanyEmployees.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IRepositoryManager _repo;
+        private readonly ILoggerManager _logger;
+
+        public WeatherForecastController(ILoggerManager logger, IRepositoryManager repo)
+        {
+            _logger = logger;
+            _repo = repo;
+        }
+
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    _logger.LogInfo("Here is info message from our values controller.");
+        //    _logger.LogDebug("Here is debug message from our values controller.");
+        //    _logger.LogWarn("Here is warn message from our values controller.");
+        //    _logger.LogError("Here is an error message from our values controller.");
+        //    return new string[] { "value 1", "value 2" };
+        //}
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            //_repo.Company.AnyMethodFromCompanyRepository();
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ Repository/RepositoryManager.cs
@@ -0,0 +1,40 @@
+﻿using Contracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private RepositoryContext _repoContext;
+        private ICompanyRepository _companyRepository;
+        private IEmployeeRepository _employeeRepository;
+        public RepositoryManager(RepositoryContext repoContext)
+        {
+            _repoContext = repoContext;
+        }
+        public ICompanyRepository Company
+        {
+            get
+            {
+                if (_companyRepository == null)
+                    _companyRepository = new CompanyRepository(_repoContext);
+                return _companyRepository;
+            }
+        }
+
+        public IEmployeeRepository Employee
+        {
+            get
+            {
+                if (_employeeRepository == null)
+                    _employeeRepository = new EmployeeRepository(_repoContext);
+                return _employeeRepository;
+            }
+        }
+
+        public void Save() => _repoContext.SaveChanges();
+    }
+}
