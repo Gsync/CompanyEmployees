01af173ce16b593e76530be21642fe224e423574
Khuram Niaz <khuram.niaz@vergeag.com>
2020-06-01 18:04:52 -0600

    get companies with dto

---
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Entities.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CompanyEmployees.Controllers
+{
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly IRepositoryManager _repo;
+        private readonly ILoggerManager _logger;
+
+        public CompaniesController(IRepositoryManager repo, ILoggerManager logger)
+        {
+            _repo = repo;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            try
+            {
+                var companies = _repo.Company.GetAllCompanies(trackChanges: false);
+
+                var companiesDto = companies.Select(c => new CompanyDto
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    FullAddress = string.Join(' ', c.Address, c.Country)
+                }).ToList();
+                return Ok(companiesDto);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetCompanies)} action {ex}");
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+    }
+}
